7be6b75bc93a20408f6f3fa81a193cc16ecad51b
Joey Gaitan <joeygaitan707@gmail.com>
2020-01-29 14:30:42 -0800

    added in working functions

--- histogram.py
+++ histogram.py
@@ -1,5 +1,14 @@
+import os
+import sys
+
+import time as t
+
+start_time = t.time()
+
 words = open('words.txt','r')
 words = ''.join(words.readlines()).split()
+
+word = sys.argv[1:]
 
 def histogram(words):
     histo = {'histoCounter':0}
@@ -17,7 +26,16 @@ def histogram(words):
 def unique_word(count):
     return (f'the amount of unique words are {count}')
 
-def frequency():
+def frequency(word):
+    histograms = histogram(words)
     
+    if word in histograms:
+        print(f'Word: {word}, Count: {histograms[word]}') 
 
-print(histogram(words))
+end_time = t.time()
+    
+print(f"The program ran for {end_time - start_time} seconds")
+
+# print(histogram(words))
+
+print(frequency(word[0]))
