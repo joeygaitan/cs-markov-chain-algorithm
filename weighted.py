7b4486f810fdfb2b26db837ff2268713def0e3bd
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-05 00:52:51 -0800

    figured out the weight function. Now moving onto the flask application. Flask....

--- weighted.py
+++ weighted.py
@@ -9,7 +9,7 @@ def total(list):
       total += element
     return total
                
-def sample_by_frequency(histogram):
+def sample_by_frequency(word_counts):
     random_index = random.randint(0, total(word_counts.values()) - 1)
     # TODO: select a word based on frequency
     #how can we sample words using observed frequencies?
@@ -17,9 +17,15 @@ def sample_by_frequency(histogram):
     for (word, count) in word_counts.items():
       end = start + count
       if end >= random_index and random_index >= start:
+        print(weight(word, word_counts))
         return word
       else:
         start = end
+    print(weight(word, word_counts))
     return word
+
+def weight(word, histogram):
+    mass = histogram[word] / total(word_counts.values())
+    return f'{word} => {mass}'
     
 print(sample_by_frequency(word_counts))
