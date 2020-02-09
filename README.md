859aac813032d7d677b9fca378b8e9a7fbf4e455
Joey Gaitan <joeygaitan707@gmail.com>
2020-02-08 23:58:21 -0800

    added in a modified linkedlist working on displaying text to heroku

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Hopefully setup with React Frontend, and api flask application hosted with gundb
 It is possible https://github.com/feat7/blog-writer
 
 Surge way https://dbader.org/blog/open-source-tools-to-generate-and-host-your-api-documentation-for-free
+
+## Markov Chain Algorithm Site
+
+Installion process
+
+```
+pip3 install -r requirements.txt
+```
+
+python run 
+
--- algorithms/LinkedLists.py
+++ algorithms/LinkedLists.py
@@ -1,3 +1,6 @@
+import time as t
+
+import
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
@@ -16,11 +19,43 @@ class linkedList:
     def displayAll(self):
         current = self.head
 
-        print(current.data)
-        print(current.next)
-        print(current.data)
+        while(current):
+            print(current.data)
+            current = current.next
+    
+    def displayOne(self,index):
+        current = self.head
+
+        if current.size < index:
+            print("out of scope")
+        while(current.next != None):
+            if current.data == index:
+                print(current.data)
+            current = current.next 
+
+    def addLast(self,data):
+        node = Node(data)
+        current = None
+
+        if self.head == None:
+            self.head = node 
+        else:
+            current = self.head
+
+            while(current.next != None):
+                current = current.next
+            current.next = node
+        
+        self.size += 1
+    
+    def insertAt(self,index,data):
+        node = Node(data)
+
         
 
+
+
+start_time = t.time()
 
 ll = linkedList()
 
@@ -28,5 +63,14 @@ ll.addFirst(400)
 ll.addFirst(300)
 ll.addFirst(200)
 ll.addFirst(100)
+ll.addLast(1000)
+
+print(ll.size)
+
 
 ll.displayAll()
+
+end_time = t.time()
+
+
+print(f"The program ran for {end_time - start_time} seconds")
